1baa9a0c9ff7001178e7a1aa68d6f643d97169e9
itsahadul99 <itsahadul999@gmail.com>
2024-06-01 00:23:02 +0600

    added popular contest section

--- src/components/shared/Container.jsx
+++ src/components/shared/Container.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 const Container = ({ children }) => {
-    return (
-      <div className='max-w-7xl mx-auto xl:px-10 lg:px-8 sm:px-2 px-4'>
-        {children}
-      </div>
-    )
-  }
-  
-  export default Container
+  return (
+    <div className='max-w-7xl mx-auto xl:px-10 lg:px-8 sm:px-2 px-4 xl:py-10 lg:py-8 sm:py-2 py-4 '>
+      {children}
+    </div>
+  )
+}
+
+export default Container
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -70,24 +70,27 @@ const Navbar = () => {
                 </div>
                 <div className="navbar-end">
                     <div className="flex justify-center gap-3 items-center">
+                        <div className="hidden lg:block">
+                            <ul className="flex gap-5 items-center mr-3">
+                                {links}
+                            </ul>
+                        </div>
                         <div className="dropdown dropdown-end">
                             {
-                                user ? <div className="dropdown dropdown-end">
-                                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                                        <div className="w-10 rounded-full">
-                                            <img alt="User pic" src={user?.photoURL} />
+                                user ?
+                                    <div className="dropdown dropdown-end">
+                                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                            <div className="w-10 rounded-full">
+                                                <img alt="User pic" src={user?.photoURL} />
+                                            </div>
+                                        </div>
+                                        <div tabIndex={0} className="mt-3 z-20 bg-base-200 space-y-2 shadow menu menu-sm dropdown-content rounded-box w-52 font-bold *:p-2">
+                                            <p className="w-full hover:bg-gray-200">Username: {user?.displayName}</p>
+                                            <button className="hover:bg-gray-200" onClick={handleLogOut}>Logout</button>
                                         </div>
                                     </div>
-                                    <div tabIndex={0} className="mt-3 z-20 bg-base-200 space-y-2 shadow menu menu-sm dropdown-content rounded-box w-52 font-bold *:p-2">
-                                        <p className="w-full hover:bg-gray-200">Username: {user?.displayName}</p>
-                                        <button className="hover:bg-gray-200" onClick={handleLogOut}>Logout</button>
-                                    </div>
-                                </div>
                                     : <>
                                         <div className="flex gap-2 items-center">
-                                            <ul className="flex gap-5 items-center mr-3">
-                                                {links}
-                                            </ul>
                                             <Link to="login"><button className="btn btn-outline btn-xs hover:bg-secondary lg:btn-sm border-primary text-primary">LogIn </button></Link>
                                         </div>
                                     </>
@@ -95,7 +98,7 @@ const Navbar = () => {
                         </div>
                         <div>
                             <label className="cursor-pointer grid place-items-center">
-                                <input onClick={handleToggle} type="checkbox" value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
+                                <input onClick={handleToggle} type="checkbox" value="dark]" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
                                 <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
                                 <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
                             </label>
