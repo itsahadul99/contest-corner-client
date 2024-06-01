c3d1775478048aa945f7ab77c23b84e48b805364
itsahadul99 <itsahadul999@gmail.com>
2024-06-01 12:10:51 +0600

    added footer

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/shared/Footer";
+import Navbar from "../components/shared/Navbar";
 
 const Main = () => {
     return (
         <div>
+            <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,11 +1,10 @@
-import Navbar from "../../components/shared/Navbar";
+
 import Banner from "./Banner";
 import PopularContest from "./PopularContest";
 
 const Home = () => {
     return (
         <div>
-            <Navbar />
             <Banner />
             <PopularContest />
         </div>
